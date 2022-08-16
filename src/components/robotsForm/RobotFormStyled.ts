94998895ac74368cba2a5c85e58c797beb9da2d0
Fernando Paulino <xizes2@hotmail.com>
2022-08-16 22:31:01 +0200

    create login component, install jsonwebtoken

---
+++ src/components/robotsForm/RobotFormStyled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const RobotFormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+
+  .form-container {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export default RobotFormStyled;
